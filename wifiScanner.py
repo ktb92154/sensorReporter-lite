59cc7a33022cc39f89cb0c8d617ecaabcb5ff6f7
Sascha Sambale <mastixmc@gmail.com>
2016-06-14 07:29:40 +0200

    Fixed missing section type.

--- wifiScanner.py
+++ wifiScanner.py
@@ -24,7 +24,6 @@ class WifiSensor:
         """Finds whether the BT device is close and publishes its current state"""
 
         self.logger = logger
-        section = "Sensor"
         self.name = config.get(section, "Name")
         self.address = config.get(section, "Address")
         self.destination = config.get(section, "Destination")
